b045c5661e6d640f005ca57bd85dea117c6a6c5e
Kadijatu Conteh <kconteh191@gmail.com>
2023-10-26 21:25:07 +0100

    Introduce station and registered user objects, along with a new user registration method

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -2,7 +2,31 @@ const User = require('./User')
 const Scooter = require('./Scooter')
 
 class ScooterApp {
-  // ScooterApp code here
+  constructor() {
+    this.stations = {
+      "Station 1": [],
+      "Station 2": [],
+      "Station 3": [],
+    }
+    this.registeredUsers = {};
+  }
+    //ScooterApp should include methods...
+  registerUser(username, password, age) {
+    if (this.registeredUsers[username]) {
+      throw new Error("User is already registered.");
+    }
+
+    if (age < 18) {
+      throw new Error("User is too young to register.");
+    }
+    
+  };
+  
+
+
 }
+
+
+
 
 module.exports = ScooterApp
