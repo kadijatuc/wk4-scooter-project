5b3988b3a18372bdcd1282ad15ad848b386b5504
Kadijatu Conteh <kconteh191@gmail.com>
2023-10-26 20:43:43 +0100

    Added User class properties and methods with manual test

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,28 @@
+//each scooter has these properties
 class Scooter{
-  // scooter code here
+  constructor(station, user, serial, nextSerial, charge, isBroken) {
+    this.station = station;
+    this.user = user;
+    this.serial = serial.nextSerial+1;
+    this.nextSerial = 1;
+    this.charge = charge;
+    this.isBroken = isBroken;
+  }
+  //renting out a single scooter to a user
+  rent(user) {
+    if (this.charge > 20 && !this.isBroken) {
+      return this.station = null; 
+      this.user = user;
+    }else {
+      throw new Error (this.isBroken ? "scooter needs to charge" : "scooter needs repair")
+    };
+  }
+  //returning a scooter back to a station
+  dock(station) {
+    this.station = station;
+    this.user = null;
+  }
+
 }
 
 
--- src/User.js
+++ src/User.js
@@ -1,5 +1,24 @@
 class User {
-  // User code here
+  constructor(username, password, age) {
+    this.username = username;
+    this.password = password;
+    this.age = age;
+    this.loggedIn = false;
+  }
+  login(password) {
+    if (password === this.password) {
+      this.loggedIn = true;
+    }else {
+      throw new Error("Incorrect Password");
+    }
+  }
+  logout() {
+    this.loggedIn = false;
+  }
 }
+//testing the User class on console
+const u1 = new User ("xyzy", "password1", 20);
+console.log("xyzy", u1);
 
-module.exports = User
+
+module.exports = Userß
